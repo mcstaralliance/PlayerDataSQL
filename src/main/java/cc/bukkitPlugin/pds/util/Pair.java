33199bd705016ebbf2bb72f98cf9a55767849393
聪聪 <178666380@qq.com>
2019-01-26 16:35:33 +0800

    增加lib

---
+++ src/main/java/cc/bukkitPlugin/pds/util/Pair.java
@@ -0,0 +1,27 @@
+package cc.bukkitPlugin.pds.util;
+
+import lombok.Getter;
+
+public class Pair<K, V> {
+
+    @Getter
+    private final K key;
+    @Getter
+    private final V value;
+
+    public Pair(K pKey, V pValue) {
+        this.key = pKey;
+        this.value = pValue;
+    }
+    
+    @Override
+    public String toString() {
+        return this.getKey()+" = "+this.getValue();
+    }
+    
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+}
