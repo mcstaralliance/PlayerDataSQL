c482b47b9a2f5bdb38852b1144642dff5de9016f
聪聪 <178666380@qq.com>
2019-01-26 16:37:55 +0800

    增加饰品(1.12.2)数据同步

---
+++ src/main/java/cc/bukkitPlugin/pds/dmodel/v1_12_2/DM_Baubles.java
@@ -0,0 +1,39 @@
+package cc.bukkitPlugin.pds.dmodel.v1_12_2;
+
+import java.lang.reflect.Method;
+
+import cc.bukkitPlugin.pds.PlayerDataSQL;
+
+public class DM_Baubles extends ADM_ForgeCapabilityProvider {
+
+    private Method method_BaublesApi_getBaubles = null;
+    private Method method_InventoryBaubles_readNBT = null;
+    private Method method_InventoryBaubles_saveNBT = null;
+    private Method method_InventoryBaubles_syncSlotToClients = null;
+
+    private Boolean mInit = null;
+
+    public DM_Baubles(PlayerDataSQL pPlugin) {
+        super(pPlugin);
+
+        this.addCapabilityP("baubles.api.cap.BaublesContainerProvider");
+    }
+
+    @Override
+    public String getModelId() {
+        return "Baubles_v1_12_2";
+    }
+
+    @Override
+    public String getDesc() {
+        return "饰品背包";
+    }
+
+    @Override
+    public boolean initOnce() throws Exception {
+        Class.forName("baubles.common.Baubles");
+
+        return super.initOnce();
+    }
+
+}
