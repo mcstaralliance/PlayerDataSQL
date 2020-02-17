6692f65a5fd88ec00049936ac42778f082ff45e2
聪聪 <178666380@qq.com>
2020-02-18 00:03:38 +0800

    增加同步到客户端代码

--- src/main/java/cc/bukkitPlugin/pds/dmodel/v1_12_2/DM_AcademyCraft.java
+++ src/main/java/cc/bukkitPlugin/pds/dmodel/v1_12_2/DM_AcademyCraft.java
@@ -1,8 +1,23 @@
 package cc.bukkitPlugin.pds.dmodel.v1_12_2;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import cc.bukkitPlugin.commons.nmsutil.NMSUtil;
 import cc.bukkitPlugin.pds.PlayerDataSQL;
+import cc.bukkitPlugin.pds.util.CPlayer;
+import cc.bukkitPlugin.pds.util.CapabilityHelper;
+import cc.commons.util.reflect.FieldUtil;
+import cc.commons.util.reflect.MethodUtil;
 
 public class DM_AcademyCraft extends ADM_CapabilityProvider {
+
+    /** private final ImmutableMap<Class, DataPart> constructed; */
+    private Field field_EntityData_constructed;
+    /** private void onQuerySync(EntityPlayerMP) */
+    private Method method_DataPart_onQuerySync;
+    private Object DATA_PART_CAPABILITY;
 
     public DM_AcademyCraft(PlayerDataSQL pPlugin) {
         super(pPlugin);
@@ -19,6 +34,29 @@ public class DM_AcademyCraft extends ADM_CapabilityProvider {
     @Override
     public String getDesc() {
         return "学园都市超能力";
+    }
+
+    @Override
+    protected boolean initCapability() throws Exception {
+        Class<?> tClazz = Class.forName("cn.lambdalib2.datapart.EntityData");
+        this.field_EntityData_constructed = FieldUtil.getDeclaredField(tClazz, "constructed");
+
+        tClazz = Class.forName("cn.lambdalib2.datapart.DataPart");
+        this.method_DataPart_onQuerySync = MethodUtil.getDeclaredMethod(tClazz, "onQuerySync", NMSUtil.clazz_EntityPlayerMP);
+
+        tClazz = Class.forName("cn.lambdalib2.datapart.CapDataPartHandler");
+        DATA_PART_CAPABILITY = FieldUtil.getStaticDeclaredFieldValue(tClazz, "DATA_PART_CAPABILITY");
+        return true;
+    }
+
+    @Override
+    public void updateAround(CPlayer pPlayer, Class<?> pProvider) {
+        Object tNMSPlayer = pPlayer.getNMSPlayer();
+        Object tEntityData = CapabilityHelper.getCapability(tNMSPlayer, DATA_PART_CAPABILITY, null);
+        if (tEntityData == null) return;
+        for (Object sObj : ((Map)FieldUtil.getFieldValue(field_EntityData_constructed, tEntityData)).values()) {
+            MethodUtil.invokeMethod(method_DataPart_onQuerySync, sObj, tNMSPlayer);
+        }
     }
 
 }
