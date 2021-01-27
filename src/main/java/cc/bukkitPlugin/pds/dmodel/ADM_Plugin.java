9fc6aa8ed7bbcac43cd1f97765318ec8fa550042
聪聪 <178666380@qq.com>
2021-01-27 20:19:52 +0800

    插件模板初版

---
+++ src/main/java/cc/bukkitPlugin/pds/dmodel/ADM_Plugin.java
@@ -0,0 +1,39 @@
+package cc.bukkitPlugin.pds.dmodel;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import cc.bukkitPlugin.pds.PlayerDataSQL;
+
+public abstract class ADM_Plugin extends ADataModel {
+
+    protected final String mPlugin;
+
+    public ADM_Plugin(PlayerDataSQL pMain, String pPluginName) {
+        super(pMain);
+
+        this.mPlugin = pPluginName;
+    }
+
+    @Override
+    public String getModelId() {
+        return this.mPlugin;
+    }
+
+    public String getDependentPluginN() {
+        return this.mPlugin;
+    }
+
+    @Nullable
+    public Plugin getDependentPluginI() {
+        return Bukkit.getPluginManager().getPlugin(this.getDependentPluginN());
+    }
+
+    @Override
+    protected boolean initOnce() throws Exception {
+        return Bukkit.getPluginManager().isPluginEnabled("DeluxeTags");
+    }
+
+}
